50e64f957932be068736df94aa86cc4e9d2a6aaa
Fernando Rodríguez <frr@keepcoding.io>
2021-06-08 13:08:30 +0200

    fresh from dart

---
+++ lib/mow.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mow;
+
+export 'src/mow_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
