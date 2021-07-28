6f6764ea85936ac66c6f971630c41a814a44ea5d
Fernando Rodríguez <frr@keepcoding.io>
2021-07-28 20:19:25 +0200

    renamed notifications to notice

--- lib/mow.dart
+++ lib/mow.dart
@@ -4,7 +4,7 @@
 library mow;
 
 export 'package:updatable/updatable.dart';
-export 'package:notifications/notifications.dart';
+export 'package:notice_service/notice_service.dart';
 
 export 'src/model_widget.dart';
 export 'src/observer_state.dart';
